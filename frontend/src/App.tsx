a2ef29dd9fc76ac6bed13c6073a50d09904814fe
Fran Zekan <zekan.fran369@gmail.com>
2020-07-20 23:48:50 +0200

    Bigger fonts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,17 +5,26 @@ import { SWRConfig } from "swr";
 import Home from "./pages/Home";
 import PersonShow from "./pages/person/Show";
 import fetcher from "./util/fetch";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 90%;
+  max-width: 1000px;
+  margin: 0 auto;
+`;
 
 const App: React.FC = () => {
   return (
-    <SWRConfig value={{ fetcher, suspense: true }}>
-      <BrowserRouter>
-        <Switch>
-          <Route path="/person/:name" component={PersonShow} />
-          <Route path="/" component={Home} />
-        </Switch>
-      </BrowserRouter>
-    </SWRConfig>
+    <Container>
+      <SWRConfig value={{ fetcher, suspense: true }}>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/person/:name" component={PersonShow} />
+            <Route path="/" component={Home} />
+          </Switch>
+        </BrowserRouter>
+      </SWRConfig>
+    </Container>
   );
 };
 
--- frontend/src/components/Table.tsx
+++ frontend/src/components/Table.tsx
@@ -4,12 +4,13 @@ const Table = styled.table`
   td,
   th {
     padding: 0.5em;
-    font-size: 1em;
+    font-size: 1.25em;
     word-break: break-all;
+    text-align: left;
   }
 
   th {
-    font-size: 1.25em;
+    font-size: 1.5em;
     font-weight: 700;
   }
 `;
