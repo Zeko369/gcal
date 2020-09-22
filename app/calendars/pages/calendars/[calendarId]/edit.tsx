fc9c7f2bd1b14609f80aaf0a5de15731c7c3969d
Fran Zekan <zekan.fran369@gmail.com>
2020-09-22 23:17:16 +0200

    Better edit

--- app/calendars/pages/calendars/[calendarId]/edit.tsx
+++ app/calendars/pages/calendars/[calendarId]/edit.tsx
@@ -3,36 +3,32 @@ import Layout from "app/layouts/Layout"
 import { Head, Link, useRouter, useQuery, useParam, BlitzPage } from "blitz"
 import getCalendar from "app/calendars/queries/getCalendar"
 import updateCalendar from "app/calendars/mutations/updateCalendar"
-import CalendarForm from "app/calendars/components/CalendarForm"
+import { CalendarForm, CalendarFormData } from "app/calendars/components/CalendarForm"
+import { Heading } from "@chakra-ui/core"
 
 export const EditCalendar = () => {
   const router = useRouter()
   const calendarId = useParam("calendarId", "number")
   const [calendar, { mutate }] = useQuery(getCalendar, { where: { id: calendarId } })
 
-  return (
-    <div>
-      <h1>Edit Calendar {calendar.id}</h1>
-      <pre>{JSON.stringify(calendar)}</pre>
+  const onSubmit = async (data: CalendarFormData) => {
+    try {
+      const updated = await updateCalendar({
+        where: { id: calendar.id },
+        data,
+      })
+      mutate(updated)
+      router.push("/calendars/[calendarId]", `/calendars/${updated.id}`)
+    } catch (error) {
+      console.error(error)
+    }
+  }
 
-      <CalendarForm
-        initialValues={calendar}
-        onSubmit={async () => {
-          try {
-            const updated = await updateCalendar({
-              where: { id: calendar.id },
-              data: { name: "MyNewName" },
-            })
-            mutate(updated)
-            alert("Success!" + JSON.stringify(updated))
-            router.push("/calendars/[calendarId]", `/calendars/${updated.id}`)
-          } catch (error) {
-            console.log(error)
-            alert("Error creating calendar " + JSON.stringify(error, null, 2))
-          }
-        }}
-      />
-    </div>
+  return (
+    <>
+      <Heading>Edit Calendar {calendar.id}</Heading>
+      <CalendarForm initialValues={calendar} onSubmit={onSubmit} />
+    </>
   )
 }
 
