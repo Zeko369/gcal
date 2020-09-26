f0b8fd84cb3b44c93e73a756e0438542fad2ebaf
Fran Zekan <zekan.fran369@gmail.com>
2020-09-26 22:37:04 +0200

    Remove old api

--- app/api/people.ts
+++
@@ -1,8 +0,0 @@
-import { NextApiHandler } from "next";
-import { people } from "../../lib/people";
-
-const getPeople: NextApiHandler = async (req, res) => {
-  res.json(people);
-};
-
-export default getPeople;
