f678715fc20dc02a45ce5db31ac94d7953f09f2d
Fran Zekan <zekan.fran369@gmail.com>
2021-01-03 22:05:19 +0100

    Fix deploy run (yarn missing)

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,4 +3,6 @@
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
+export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
+
 yarn start --production -p 3002
